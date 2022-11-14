d0e5d51f6895c38ffa319c46fa1e803a04aa0b11
math92n4 <math92n4@stud.kea.dk>
2022-11-14 13:24:06 +0100

    main method

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("Dog");
+        Cat cat = new Cat("Cat");
+
+        dog.makeSound();
+        cat.makeSound();
+
+
+    }
+}
